bac6379d5f3bc7d86420498d1f45f8e376b7bb00
UmaruDoma <schreiber@screenpaper.de>
2018-11-30 22:43:09 +0100

    Initial Upload of my Plugin
    The Plugin is built with eclipse. You have to make a Directory "jars" with the files "craftbukkit-1.12.2-R0.1-SNAPSHOT-b1479.jar" and "minecraft_server.1.12.jar" right next to the Project and reference these files.

---
+++ src/de/spc/mc/playersList.java
@@ -0,0 +1,72 @@
+package de.spc.mc;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.logging.Logger;
+
+import org.bukkit.Material;
+
+public class playersList {
+
+	private Logger log = null;
+	private Map<String, internalCommandState> playerz = new HashMap<String, internalCommandState>();
+
+	public  playersList (Logger alog)
+	{
+		log = alog;
+	}
+	
+	public void CommandOn (String cmd, String[] args, String player) 
+	{
+		CommandOff (player); 
+		internalCommandState state = new internalCommandState(log);
+		state.CommandOn(cmd,args,player);
+		playerz.put(player, state);
+	}
+	
+	public internalCommandState GetState(String player)
+	{
+		internalCommandState state;
+		if (playerz.containsKey(player)) {
+			state = playerz.get(player);
+			return state;
+		} else {
+			return null;
+		}
+	}
+	public void CommandOff (String player) 
+	{
+		if (playerz.containsKey(player)) {
+			playerz.remove(player);		
+		} 		
+	}
+	
+	boolean IsCommandActive (String player, String command)
+	{
+		
+		internalCommandState state;
+		 //log.info("internalCommandState "+ player + ", player "+ command );
+
+		if (playerz.containsKey(player)) {
+			state = playerz.get(player);
+			return state.IsCommandEq(command);
+		} else {
+			return false;
+		}
+	}
+	public Material GetMaterial (String player)
+	{	
+		internalCommandState state;
+		if (playerz.containsKey(player)) {
+			state = playerz.get(player);
+			return state.GetMateriel();
+		} else {
+			return null;
+		}
+	}
+	boolean IsMaterielRedstone(String player)
+	{
+		Material mat = GetMaterial (player);
+		return (mat.equals(Material.REDSTONE_BLOCK));		
+	}
+}
